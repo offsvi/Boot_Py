e4baa07a34eb959af6873c55cbdfb0b9e825c1ac
Vítor Oliveira <vitorsk807@gmail.com>
2023-12-18 20:03:28 -0300

    Finalizando código

--- Teste_Ping.py
+++ Teste_Ping.py
@@ -8,4 +8,13 @@ pyautogui.pause = 1
 pyautogui.press('win')
 
 pyautogui.write('PowerShell')
+pyautogui.pause = 1
 pyautogui.press('enter')
+
+pyautogui.pause = 1
+
+pyautogui.write('ping https://google.com.br')
+
+pyautogui.pause = 5
+
+pyautogui.write('exit')
