4fbd03583b40dc2fe62c1090517609d10909e7a0
Vítor Oliveira <vitorsk807@gmail.com>
2023-12-26 21:20:35 -0300

    Inserindo comentarios para organização e entendimento de cada linha

--- Teste_Ping.py
+++ Teste_Ping.py
@@ -1,9 +1,13 @@
+#Importando biblioteca Pyautogui e Time 
 import pyautogui 
 import time 
 
+# Alert irá mostrar uma janela com o texto das aspas 
 pyautogui.alert("Não mexa no computador, o código irá começar a rodar")
 
+# Pause utiliza a biblioteca time e utiliza uma pausa entre um código e outro
 pyautogui.PAUSE = 2
+
 
 pyautogui.press("win")
 
@@ -14,7 +18,7 @@ pyautogui.press("enter")
 pyautogui.PAUSE = 2
 
 pyautogui.write("ping 192.168.75.1")
-pyautogui.press("enter")
+pyautogui.press("enter") 
 
 pyautogui.PAUSE = 6 
 
